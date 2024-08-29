b7c72b1fa316ee9c97efa6a540429b8852439d92
Melanie Ehrlich <jmfsmof@jmfamily.com>
2024-08-29 14:08:10 -0400

    added pokemon api

--- codejam3-pokemon/Program.cs
+++ codejam3-pokemon/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PokeAPI;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using Microsoft.AspNetCore.Mvc;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        
+        string title = @"
+                                  ,'\
+    _.----.        ____         ,'  _\   ___    ___     ____
+_,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
+\      __    \    '-.  | /   `.  ___    |    \/    |   '-.   \ |  |
+ \.    \ \   |  __  |  |/    ,','_  `.  |          | __  |    \|  |
+   \    \/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |
+    \     ,-'/  /   \    ,'   | \/ / ,`.|         /  /   \  |     |
+     \    \ |   \_/  |   `-.  \    `'  /|  |    ||   \_/  | |\    |
+      \    \ \      /       `-.`.___,-' |  |\  /| \      /  | |   |
+       \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |
+        \_.-'       |__|    `-._ |              '-.|     '-.| |   |
+                                `'                            '-._|                                                                                       
+                                                                           ";
+
+        Console.WriteLine(title);
+
+        GetPokemon("pikachu");
+
+    }
+
+    public static async void GetPokemon(string pokemonspecies)
+    {
+
+        Console.WriteLine("hi");
+
+        //PokemonSpecies p = await DataFetcher.GetApiObject<PokemonSpecies>(395);
+
+        PokemonSpecies p = await DataFetcher.GetNamedApiObject<PokemonSpecies>(pokemonspecies);
+
+        float cRate = p.CaptureRate;
+        
+        Console.WriteLine(p.Name + " has a capture rate of " + cRate + "!");
+
+       
+    }
+
+}
